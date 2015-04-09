37cb7b17049150e66124aed4a757366611a8fd4e
Tim Preece <tdpreece@gmail.com>
2015-04-09 19:45:52 +0000

    All special strike and spare actions are done as part of first 10 normal frames calculations.  Factor out get_strike_bonus method.

--- bowling/bowling_scorer.py
+++ bowling/bowling_scorer.py
@@ -19,13 +19,16 @@ def get_score_from_line(line):
     for i, roll in enumerate(line):
         if get_frame(line[:i+1]) <= 10:
             score += get_value(roll)
-        if roll == '/':
-            score -= get_value(line[i-1])
-            score += get_value(line[i+1]) 
-        elif roll == 'X':
-            if get_frame(line[:i+1]) <= 10:
-              score += get_value(line[i+1]) + get_value(line[i+2])
+            if roll == '/':
+                score -= get_value(line[i-1])
+                score += get_value(line[i+1]) 
+            elif roll == 'X':
+                score += get_strike_bonus(line, i)
     return score
+
+def get_strike_bonus(line, current_roll_number):
+    return get_value(line[current_roll_number+1]) + get_value(line[current_roll_number+2])
+
 
 def get_frame(line):
     current_frame = 0
