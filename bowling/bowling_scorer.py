a7dc00fa079e8593efbd306c14edce81955dc8a8
Tim Preece <tdpreece@gmail.com>
2015-04-09 20:12:12 +0000

    Adding is_standard_roll, get_spare_correction and get_spare_bonus methods.

--- bowling/bowling_scorer.py
+++ bowling/bowling_scorer.py
@@ -17,14 +17,26 @@ def get_value(roll):
 def get_score_from_line(line):
     score = 0
     for i, roll in enumerate(line):
-        if get_frame(line[:i+1]) <= 10:
+        if is_standard_roll(line, i): 
             score += get_value(roll)
             if roll == '/':
-                score -= get_value(line[i-1])
-                score += get_value(line[i+1]) 
+                score -= get_spare_correction(line, i)
+                score += get_spare_bonus(line, i)
             elif roll == 'X':
                 score += get_strike_bonus(line, i)
+        else:
+            break
     return score
+
+def is_standard_roll(line, current_roll_number):
+    return get_frame(line[:current_roll_number+1]) <= 10
+
+def get_spare_correction(line, current_roll_number):
+    return get_value(line[current_roll_number-1])
+
+
+def get_spare_bonus(line, current_roll_number):
+    return get_value(line[current_roll_number+1]) 
 
 def get_strike_bonus(line, current_roll_number):
     return get_value(line[current_roll_number+1]) + get_value(line[current_roll_number+2])
